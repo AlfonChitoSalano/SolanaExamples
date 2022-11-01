6c059bb5570e33fea7a489db94de36a22262d361
Chito Salano <Alfonchitosalao2@gmail.com>
2022-11-01 14:56:54 +0800

    add wallet login and out

---
+++ PhantomWalletTest/Pages/Index.razor.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace PhantomWalletTest.Pages
+{
+    public partial class Index
+    {
+        [Inject] private IJSRuntime? JsRuntime { get; set; }
+        private DotNetObjectReference<Index>? _objRef;
+        private bool _isConnected;
+        private string _publicAddress = "";
+
+        [JSInvokable("OnConnected")]
+        public void OnConnected(string publicKey)
+        {
+            _isConnected = true;
+            _publicAddress = publicKey;
+            StateHasChanged();
+        }
+
+        [JSInvokable("OnDisConnected")]
+        public void OnDisConnected()
+        {
+            _isConnected = false;
+            _publicAddress = "";
+            StateHasChanged();
+        }
+
+        protected override void OnInitialized()
+        {
+            _objRef = DotNetObjectReference.Create(this);
+        }
+
+        private async Task ConnectAsync()
+        {
+            if (JsRuntime == null)
+            {
+                return;
+            }
+
+            await JsRuntime.InvokeVoidAsync("solanaPhantom.login", _objRef);
+        }
+
+        private async Task DisConnectAsync()
+        {
+            if (JsRuntime == null)
+            {
+                return;
+            }
+
+            await JsRuntime.InvokeVoidAsync("solanaPhantom.signOut", _objRef);
+        }
+    }
+}
---
+++ PhantomWalletTest/wwwroot/scripts/phantomWallet.js
@@ -0,0 +1,65 @@
+﻿(function () {
+    // Global export
+    window.solanaPhantom = {
+        //https://github.com/phantom-labs/sandbox/blob/b57fdd0e65ce4f01290141a01e33d17fd2f539b9/src/App.tsx#L263
+        login: async function (dotNetInstance) {
+            if (!('phantom' in window)) {
+                window.open('https://phantom.app/', '_blank');
+                return;
+            }
+
+            let provider = getProvider();
+
+            if (!(provider?.isPhantom)) {
+                console.log("wallet not phantom");
+                return;
+            }
+
+            provider.connect({ onlyIfTrusted: true })
+                .then(({ publicKey }) => {
+                    console.log("wallet is already connected");
+                    dotNetInstance.invokeMethodAsync('OnConnected', publicKey.toString());
+                })
+                .catch(() => {
+                    console.log("fail connecting");
+                });
+
+            if (provider.isConnected) {
+                return;
+            }
+
+            try {
+                console.log("connecting to wallet main");
+                let resp = await provider.connect();
+                let publicKey = resp.publicKey.toString();
+                dotNetInstance.invokeMethodAsync('OnConnected', publicKey);
+            } catch (error) {
+                console.log("error log.");
+                console.log(error);
+            }
+        },
+
+        signOut: async function (dotNetInstance) {
+            let provider = getProvider();
+
+            if (!(provider?.isPhantom)) {
+                console.log("wallet not phantom");
+                return;
+            }
+
+            await provider.disconnect();
+            console.log("wallet disconnected.");
+
+            if (provider.isConnected) {
+                console.log("wallet is not disconnected well.");
+                return;
+            }
+
+            dotNetInstance.invokeMethodAsync('OnDisConnected');
+        }
+    };
+
+    function getProvider() {
+        return window.phantom?.solana;
+    }
+})();
